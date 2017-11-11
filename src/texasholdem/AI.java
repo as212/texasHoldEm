978294c9d35d1e9bfaad20d74058efc065a63aca
adams_000 <adams_000@AdamKatelynns>
2017-11-10 18:05:44 -0600

    commit it

--- src/texasholdem/AI.java
+++ src/texasholdem/AI.java
@@ -13,19 +13,61 @@ public class AI extends Player {
     Random r = new Random();
     Random q = new Random();
     
-    public void AIturn()
+    public int AIturn(int difficulty, int minimum, AI a)
     {               
-       int choice = r.nextInt(100);
+       if (difficulty == 0)
+       {    
+        int choice = r.nextInt(100);
        
        if(choice < 90)
        {
-           subtractChips(q.nextInt(getChips()));
+           int betChips = getChips() + 1;
+           int maximum = getChips();
+           while (betChips > maximum)
+           {    
+           betChips = q.nextInt(minimum);
+           }
+           
+           subtractChips(betChips);
+           return betChips;
        }
        else
        {
            didFold = true;
            
-       }    
+       }
+       
+       }
+       else
+       {
+           checkCards c = new checkCards();
+           
+           a.sortHand(a.hand); 
+           int hvalue = c.checkCards(a.hand);
+           if ( hvalue > 15)
+           {
+                int betChips = getChips() + 1;
+           int maximum = getChips();
+           while (betChips > maximum)
+           {    
+           betChips = q.nextInt(minimum);
+           return betChips;
+           }
+           
+           subtractChips(betChips);
+       }
+           else if ( hvalue > 19)
+           {   
+               subtractChips(a.getChips());
+               return a.getChips();
+           }
+           else 
+           {
+               didFold = true;
+           }
+           
+       }
+       return 0;
     }
 
     
